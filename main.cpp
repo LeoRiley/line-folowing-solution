d977c1b262bb180a08242fd9dbfc94b89da05bf1
Leo Riley <leoriley03@gmail.com>
2023-04-30 21:38:14 +0100

    Cleaned Up Main function with coments

--- main.cpp
+++ main.cpp
@@ -29,7 +29,8 @@ using namespace std;
 // #include "main.hpp"     // You can use this file for declaring defined values
 // and functions
 
-vector<Mat> setup(void) {
+vector<Mat> setup(void)
+{
   /// Setup camera won't work if you don't have a compatible webcam
   //        setupCamera(320, 240);  // Enable the camera for OpenCV
 
@@ -46,7 +47,8 @@ vector<Mat> setup(void) {
              "images/Umbrella (Yellow Line).png"));
 
   // convert symbols to masks
-  for (int i = 0; i < symbols.size(); i++) {
+  for (int i = 0; i < symbols.size(); i++)
+  {
     inRange(symbols[i], Scalar(0, 20, 20), Scalar(180, 255, 255), symbols[i]);
     // debugDisplay(symbols[i], "symbol");
   }
@@ -56,26 +58,34 @@ vector<Mat> setup(void) {
 Mat ConvertImageForLineFollowing(Mat input);
 vector<Point> findStartAndEndPoints(cv::Mat &mask);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+  // variables defined
   vector<Mat> symbols = setup();
   Mat image, imageHSV, mask, result, BluredHSV;
+  vector<Point> points;
   int colour = 0;
-  while (1) {
-    // get image
+  // main loop for program
+  while (1)
+  {
+    // Symbol Recognition
     image = getImage(DEBUG, IMPATH);
-    cvtColor(image, imageHSV, COLOR_BGR2HSV);
-    colour = DetectLineColour(image, symbols, colour);
-    imageHSV = ConvertImageForLineFollowing(imageHSV);
-    mask = maskBasedOnColor(imageHSV, colour);
+    cvtColor(image, imageHSV, COLOR_BGR2HSV);          // Convert to HSV
+    colour = DetectLineColour(image, symbols, colour); // Detect line colour that should be followed based on symbols
+    // Line Detection
+    imageHSV = ConvertImageForLineFollowing(imageHSV); // Lowers the resolution of the image to make it easier to process
+    mask = maskBasedOnColour(imageHSV, colour);
 
-    vector<Point> points;
+    // line direction detection
     points = findStartAndEndPoints(mask);
     vector<int> angleSpeed;
     angleSpeed = calculateAngleAndOffset(points);
     // print out the angle and speed values
     printf("Angle: %d, Speed: %d\n", angleSpeed[0], angleSpeed[1]);
+    // Code to Send information to car goes Here
 
-    if (DEBUG == 1) {
+    if (DEBUG == 1)
+    {
       break;
     }
   }
@@ -83,32 +93,39 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-Mat maskBasedOnColour(int inputImage, int colour) {
+Mat maskBasedOnColour(int inputImage, int colour)
+{
   // 0 = black 1 = red 2 = green 3 = blue, 4 = yellow
   Mat mask, mask2;
-  switch (colour) {
-  case 0: {
+  switch (colour)
+  {
+  case 0:
+  {
     inRange(inputImage, Scalar(0, 0, 0), Scalar(180, 255, 50), mask);
     break;
   }
-  case 1: {
+  case 1:
+  {
     // mask the red color
     inRange(inputImage, Scalar(0, 50, 20), Scalar(30, 255, 255), mask);
     inRange(inputImage, Scalar(150, 50, 20), Scalar(180, 255, 255), mask2);
     bitwise_or(mask, mask2, mask);
     break;
   }
-  case 2: {
+  case 2:
+  {
     // mask green colour
     inRange(inputImage, Scalar(30, 50, 20), Scalar(90, 255, 255), mask);
     break;
   }
-  case 3: {
+  case 3:
+  {
     // mask blue colour
     inRange(inputImage, Scalar(90, 50, 20), Scalar(130, 255, 255), mask);
     break;
   }
-  case 4: {
+  case 4:
+  {
     // mask yellow colour
     inRange(inputImage, Scalar(10, 50, 20), Scalar(40, 255, 255), mask);
     break;
@@ -117,7 +134,8 @@ Mat maskBasedOnColour(int inputImage, int colour) {
   return mask;
 }
 
-int DetectLineColour(Mat image, vector<Mat> symbols, int previousColour) {
+int DetectLineColour(Mat image, vector<Mat> symbols, int previousColour)
+{
   // change perspective
   cvtColor(image, image, COLOR_BGR2HSV);
   // isolate pink colour from image
@@ -135,7 +153,8 @@ int DetectLineColour(Mat image, vector<Mat> symbols, int previousColour) {
   double epsilon = 0.1 * arcLength(contours[0], true);
   vector<Point> fourCorners;
   approxPolyDP(contours[0], fourCorners, epsilon, true);
-  if (fourCorners.size() != 4) {
+  if (fourCorners.size() != 4)
+  {
     printf("no corners found\n");
     return previousColour;
   }
@@ -143,10 +162,13 @@ int DetectLineColour(Mat image, vector<Mat> symbols, int previousColour) {
 
   printf("image is of size %d,%d\n", symbol.rows, symbol.cols);
   printf("symbol is of size %d,%d\n", symbols[0].rows, symbols[0].cols);
-  for (int j = 0; j < 4; j++) {
-    for (int i = 0; i < symbols.size(); i++) {
+  for (int j = 0; j < 4; j++)
+  {
+    for (int i = 0; i < symbols.size(); i++)
+    {
       float matchPercent = compareImages(symbol, symbols[i]);
-      if (matchPercent > 80) {
+      if (matchPercent > 80)
+      {
         debugDisplay(symbols[i], "symbol match");
         return i;
       }
@@ -158,7 +180,8 @@ int DetectLineColour(Mat image, vector<Mat> symbols, int previousColour) {
   return 1;
 }
 // given to us
-float compareImages(Mat cameraImage, Mat librarySymbol) {
+float compareImages(Mat cameraImage, Mat librarySymbol)
+{
   float matchPercent =
       100 - (100 / ((float)librarySymbol.cols * (float)librarySymbol.rows) *
              (2 * (float)countNonZero(
@@ -167,20 +190,24 @@ float compareImages(Mat cameraImage, Mat librarySymbol) {
   return matchPercent;
 }
 
-void debugDisplay(Mat image, String title) {
-  if (DEBUG == 1) {
+void debugDisplay(Mat image, String title)
+{
+  if (DEBUG == 1)
+  {
     imshow(title, image);
     waitKey(0);
   }
 }
 
-vector<Point> findStartAndEndPoints(Mat &mask) {
+vector<Point> findStartAndEndPoints(Mat &mask)
+{
   // Detect lines in the mask using Hough Line Transform
   vector<Vec4i> lines;
   vector<Point> points;
   cv::HoughLinesP(mask, lines, 1, CV_PI / 180, 50, 50, 10);
 
-  if (!lines.empty()) {
+  if (!lines.empty())
+  {
     Point start(lines[0][0], lines[0][1]);
     Point end(lines[0][2], lines[0][3]);
     points.assign({start, end});
@@ -193,13 +220,15 @@ vector<Point> findStartAndEndPoints(Mat &mask) {
   return points;
 }
 
-vector<int> calculateAngleAndOffset(vector<Point> points) {
+vector<int> calculateAngleAndOffset(vector<Point> points)
+{
   // calculate angle and offset
   // point 0 is at the bottom of the image
 
   // work out which point is further down the image
 
-  if (points[0].y > points[1].y) {
+  if (points[0].y > points[1].y)
+  {
     // swap points
     Point temp = points[0];
     points[0] = points[1];
@@ -211,7 +240,8 @@ vector<int> calculateAngleAndOffset(vector<Point> points) {
   int y2 = points[1].y;
   // calculate if the line goes left or right
   bool goingLeft = false;
-  if (x1 < x2) {
+  if (x1 < x2)
+  {
     goingLeft = true;
   }
 
@@ -219,12 +249,14 @@ vector<int> calculateAngleAndOffset(vector<Point> points) {
   int offset = abs(x2 - IMAGEWIDTH / 2);
   vector<int> angleAndOffset;
   angleAndOffset.assign({angle, offset});
-  if (DEBUG == 1) {
+  if (DEBUG == 1)
+  {
     printf("x1: %d, y1: %d, x2: %d, y2: %d\n", x1, y1, x2, y2);
     printf("Angle: %d\nOffset: %d\n", angle, offset);
     printf("line going left = %d\n", goingLeft);
   }
-  if (goingLeft) {
+  if (goingLeft)
+  {
     angle = -angle;
   }
 
@@ -238,33 +270,41 @@ vector<int> calculateAngleAndOffset(vector<Point> points) {
   return angleSpeed;
 }
 
-vector<Point> ContoursEdgeMatch(vector<Point> Contour) {
+vector<Point> ContoursEdgeMatch(vector<Point> Contour)
+{
   // describe this function
   // find the corners of the largest contour
-  for (int i = 0; i < Contour.size(); i++) {
+  for (int i = 0; i < Contour.size(); i++)
+  {
     if (Contour[i].x == 0 || Contour[i].x == 500 || Contour[i].y == 0 ||
-        Contour[i].y == 500) {
+        Contour[i].y == 500)
+    {
       Contour.erase(Contour.begin() + i);
     }
   }
   return Contour;
 }
 
-Mat getImage(int debug, string debugImage) {
+Mat getImage(int debug, string debugImage)
+{
   //   gets an image from the camera or if debug = 1 gets an image from a
   //   file
   //   returns the image as a matrix
   Mat image;
-  if (debug == 0) {
+  if (debug == 0)
+  {
     image = captureFrame();
-  } else {
+  }
+  else
+  {
     image = imread(debugImage, IMREAD_COLOR);
   }
   // flip image vertically
   flip(image, image, -1);
   // image.convertTo(image, -1, 2, 0);
 
-  if (debug == 1) {
+  if (debug == 1)
+  {
     imshow("base image", image);
     waitKey(0);
   }
@@ -272,7 +312,8 @@ Mat getImage(int debug, string debugImage) {
   return image;
 }
 
-Mat ConvertImageForLineFollowing(Mat input) {
+Mat ConvertImageForLineFollowing(Mat input)
+{
   Mat output;
   output = cropImageTop(output);
   resize(input, output, Size(IMAGEWIDTH, IMAGEHEIGHT), INTER_LINEAR);
@@ -280,30 +321,26 @@ Mat ConvertImageForLineFollowing(Mat input) {
   return output;
 }
 
-Mat cropImageTop(Mat image) {
+Mat cropImageTop(Mat image)
+{
   // crop the image from the top
 
   // Define the region of interest (ROI) as a rectangle that covers the top
   // portion of the image
-  int x = 0; // x-coordinate of the top-left corner of the rectangle
-  int y =
-      image.rows * 0.4; // y-coordinate of the top-left corner of the rectangle
-  int width = image.cols; // width of the rectangle
-  int height = image.rows -
-               y; // height of the rectangle, adjust the percentage as needed
+  int x = 0;                   // x-coordinate of the top-left corner of the rectangle
+  int y = image.rows * 0.4;    // y-coordinate of the top-left corner of the rectangle
+  int width = image.cols;      // width of the rectangle
+  int height = image.rows - y; // height of the rectangle, adjust the percentage as needed
   Rect roi(x, y, width, height);
 
   // Crop the image using the ROI
   Mat cropped = image(roi);
 
-  // Display the cropped image
-
-  // imshow("Cropped Image", cropped);
-  // waitKey(0);
   return cropped;
 }
 
-Mat isolateSymbol(vector<Point> corners, Mat image) {
+Mat isolateSymbol(vector<Point> corners, Mat image)
+{
   Mat warped;
   int imagewidth = 350;
   int imageheight = 350;
@@ -311,7 +348,8 @@ Mat isolateSymbol(vector<Point> corners, Mat image) {
 
   // convterting corners to point2f
   Point2f first[4];
-  for (int i = 0; i < corners.size(); i++) {
+  for (int i = 0; i < corners.size(); i++)
+  {
     first[i] = static_cast<Point2f>(corners[i]);
   }
 
@@ -324,7 +362,8 @@ Mat isolateSymbol(vector<Point> corners, Mat image) {
   return warped;
 }
 
-Mat perspectiveWarp(Mat image) {
+Mat perspectiveWarp(Mat image)
+{
   int widthnumber = 500;
   // Define the source and destination points for the perspective transform
   Point2f first[] = {Point2f(0, 0), Point2f(image.cols - 1, 0),
@@ -355,14 +394,16 @@ Mat perspectiveWarp(Mat image) {
 }
 
 // calculate line angle from mask
-double lineAngle(Mat mask) {
+double lineAngle(Mat mask)
+{
   // find the line
   vector<Vec4i> lines;
   HoughLinesP(mask, lines, 1, CV_PI / 180, 50, 50, 10);
   // draw the lines
   Mat lineImage = Mat::zeros(mask.size(), CV_8UC3);
   int x = 0;
-  for (size_t i = 0; i < lines.size(); i++) {
+  for (size_t i = 0; i < lines.size(); i++)
+  {
     x += 5;
     Vec4i l = lines[i];
     line(lineImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(x, 255, 255),
@@ -371,13 +412,15 @@ double lineAngle(Mat mask) {
   // find the angle
   double angle = 0;
   // loop through each line in lines
-  for (size_t i = 0; i < lines.size(); i++) {
+  for (size_t i = 0; i < lines.size(); i++)
+  {
     Vec4i l = lines[i];
     angle += atan2(l[3] - l[1], l[2] - l[0]) * 180 / CV_PI;
   }
   angle = angle / lines.size();
   // display the line image
-  if (DEBUG == 1) {
+  if (DEBUG == 1)
+  {
     imshow("lineImage", lineImage);
     waitKey(0);
   }
